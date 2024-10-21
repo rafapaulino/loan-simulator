345132d5d20754019c44124b4e8d9a6f3fca12d4
Rafael Paulino <rafaelpaulino@MacBook-Air-de-Rafael.local>
2024-10-21 09:01:33 -0300

    iniciei a criacao do controlador

--- app/Http/Controllers/SimulationController.php
+++ app/Http/Controllers/SimulationController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class SimulationController extends Controller
 {
-    //
+    public function simulation(Request $request)
+    {
+        return response()->json([
+            'total' => 0,
+            'parcel' => 0,
+            'fees' => 0,
+        ]);
+    }
 }
